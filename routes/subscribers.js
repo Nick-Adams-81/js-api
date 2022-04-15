60ff3645e32b9d3d073e00e48dc114f46ac00d38
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-15 17:29:40 -0500

    added subscriber model

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,18 +1,34 @@
 const express = require("express")
 const router = express.Router()
+const Subscriber = require("../models/subscriber")
 
 
 // get all
-router.get("/", (req, res) => {
-    res.send("hello")
+router.get("/", async (req, res) => {
+    try {
+        const subscriber = await Subscriber.find()
+        res.json(subscriber)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 // get one
 router.get("/:id", (req, res) => {
     res.send(req.params.id)
 })
 // create one 
-router.post("/", (req, res) => {
-    
+router.post("/", async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChanel: req.body.subscribedToChanel
+    })
+
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({ messgae: err.message })
+    }
 })
 // update one
 router.patch("/:id", (req, res) => {
